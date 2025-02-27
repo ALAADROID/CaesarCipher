8d80cffd7aca7605e9560d42c1f667a11fe6fe57
ALAADROID <muhametalaa1235@gmail.com>
2025-02-27 20:37:41 +0300

    First commit - Adding Caesar Cipher program

---
+++ Caesar cipher.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+// Encryption function
+void encryptCaesar(char *text, int shift) {
+    for (int i = 0; text[i] != '\0'; i++) {
+        char c = text[i];
+        if (c >= 'A' && c <= 'Z') {
+            text[i] = (c - 'A' + shift) % 26 + 'A';
+        } else if (c >= 'a' && c <= 'z') {
+            text[i] = (c - 'a' + shift) % 26 + 'a';
+        }
+    }
+}
+
+// Decryption function
+void decryptCaesar(char *text, int shift) {
+    encryptCaesar(text, 26 - shift); // Decryption is reverse of encryption
+}
+
+int main() {
+    char text[100];
+    int shift;
+
+    printf("Enter the text to encrypt: ");
+    fgets(text, sizeof(text), stdin);
+    text[strcspn(text, "\n")] = 0; // Remove newline from input
+
+    printf("Enter the shift key: ");
+    scanf("%d", &shift);
+
+    encryptCaesar(text, shift);
+    printf("Encrypted text: %s\n", text);
+
+    decryptCaesar(text, shift);
+    printf("Decrypted text: %s\n", text);
+
+    return 0;
+}
